7f029bb4260b0bcd57c316405ac685b6dcd82172
automatic1234 <sujonsheikh3234@gmail.com>
2026-07-24 20:57:20 +0600

    Add DataPart class for file handling

---
+++ DataPart.java
@@ -0,0 +1,31 @@
+package your.package.name;
+
+public class DataPart {
+
+    private String fileName;
+    private byte[] content;
+    private String type;
+
+    public DataPart(String name, byte[] data) {
+        fileName = name;
+        content = data;
+    }
+
+    public DataPart(String name, byte[] data, String mimeType) {
+        fileName = name;
+        content = data;
+        type = mimeType;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
